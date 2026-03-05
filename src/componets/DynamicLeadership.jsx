185a0a960a298fcf66a91bacf7c3da416c468927
Adonai <adornaijayden@gmail.com>
2026-03-06 00:23:10 +0100

    adjusted names

--- src/componets/DynamicLeadership.jsx
+++ src/componets/DynamicLeadership.jsx
@@ -39,7 +39,7 @@ const coreLeaders = [
   },{
     name: "Ifeanyichukwu Isaac Boanerges",
     role: "Co-Founder & Chief Operating Officer (COO)",
-    description: "Powertech drives the operational strategy and physical footprint of the IMTBlockchain community. Leading campus logistics and on ground execution, he is directly responsible for transforming our broader Web3 vision into a highly engaged, massive student movement at the Institute of Management and Technology. From coordinating large scale tech events to optimizing day to day community operations, his leadership ensures our decentralized goals have a powerful, real-world impact.",
+    description: "He drives the operational strategy and physical footprint of the IMTBlockchain community. Leading campus logistics and on ground execution, he is directly responsible for transforming our broader Web3 vision into a highly engaged, massive student movement at the Institute of Management and Technology. From coordinating large scale tech events to optimizing day to day community operations, his leadership ensures our decentralized goals have a powerful, real-world impact.",
     realPic: powertechpic,   
     xAvatar: powertechX, 
     xLink: "https://x.com/powertechhandle"
@@ -53,7 +53,7 @@ const coreLeaders = [
     xLink: "https://x.com/austinh"
   },
   {
-    name: "Vicky uloma",
+    name: "Vicky Uloma",
     role: "Ecosystem Moderator",
     description: "Vicky manages the day to day operations of IMTBlockchain’s social and discussion platforms. She is responsible for welcoming new members, moderating conversations, and answering questions, ensuring our digital ecosystem remains a safe, organized, and highly active hub for blockchain education.",
     realPic: vickypic,   
